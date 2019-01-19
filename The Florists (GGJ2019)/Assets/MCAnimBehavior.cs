da37e5efcd9cd68e36ca68b720593228a496a53f
Hunt J <huntehlaboratories@gmail.com>
2019-01-19 12:58:09 -0500

    Created Animator

---
+++ The Florists (GGJ2019)/Assets/MCAnimBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCAnimBehavior : MonoBehaviour {
+    private Animator anim;
+
+    void Start() {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Update() {
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+            anim.SetBool("Moving", true);
+        else
+            anim.SetBool("Moving", false);
+    }
+}
