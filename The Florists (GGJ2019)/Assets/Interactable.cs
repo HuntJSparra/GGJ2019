18fac36a16e570f2adde66aef47f3f5f52e77030
JordanDeagan <jdeags@gmail.com>
2019-01-19 15:14:55 -0500

    Abstract Interactables

--- The Florists (GGJ2019)/Assets/Interactable.cs
+++ The Florists (GGJ2019)/Assets/Interactable.cs
@@ -1,7 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public interface Interactable {
-    void receiveControl(Freeroam p);
+abstract public class Interactable : MonoBehaviour{
+	public GameObject textBubble; 
+
+    public void receiveControl(Freeroam p){
+    	StartCoroutine(run(p));
+    }
+
+    public abstract IEnumerator run(Freeroam p);
+
+    public IEnumerator showText(string display){
+    	textBubble.SetActive(true);                                             //Show text
+        textBubble.transform.Find("Text").GetComponent<Text>().text = display;
+        yield return new WaitForFixedUpdate();                                  //clear Input
+        yield return new WaitUntil(() => Input.GetButtonDown("Interact"));      //Wait for new input
+        textBubble.SetActive(false);   
+    }
 }
--- The Florists (GGJ2019)/Assets/TestInteractions.cs
+++ The Florists (GGJ2019)/Assets/TestInteractions.cs
@@ -3,31 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TestInteractions : MonoBehaviour, Interactable {
-    public GameObject textBubble; 
+public class TestInteractions : Interactable {
 
-
-
-    public void receiveControl(Freeroam p) {
-        StartCoroutine(run(p));
-        // print("ran");
-    }
-
-    public IEnumerator run(Freeroam p) {
-        p.freeze();                                 //freeze the player
+    override public IEnumerator run(Freeroam p) {
+        p.freeze();                                          //freeze the player
         // print("frozen");
-        yield return StartCoroutine(showText());    //Wait for display interactions
-        p.unfreeze();                               //unfreeze after display is finished
+        yield return StartCoroutine(showText("Test text"));  //Wait for display interactions
+        p.unfreeze();                                        //unfreeze after display is finished
         // print("unfrozen");
-    }
-
-    public IEnumerator showText(){
-        textBubble.SetActive(true);                 //Show text
-        // print("true");
-        textBubble.transform.Find("Text").GetComponent<Text>().text = "Test text";
-        yield return new WaitForFixedUpdate();      //clear Input
-        yield return new WaitUntil(() => Input.GetButtonDown("Interact")); //Wait for new input
-        // print("exit");
-        textBubble.SetActive(false);                //clear display
     }
 }
