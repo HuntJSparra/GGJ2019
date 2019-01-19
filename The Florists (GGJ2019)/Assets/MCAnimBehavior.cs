e462691bb94493351cd66853e0d837671e409f37
Hunt J <huntehlaboratories@gmail.com>
2019-01-19 13:02:00 -0500

    Changed it to velocity

--- The Florists (GGJ2019)/Assets/MCAnimBehavior.cs
+++ The Florists (GGJ2019)/Assets/MCAnimBehavior.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class MCAnimBehavior : MonoBehaviour {
     private Animator anim;
 
+    private Rigidbody2D rb;
+
     void Start() {
         anim = gameObject.GetComponent<Animator>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update() {
-        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        if (Mathf.Abs(rb.velocity.x) > 0.1 || Mathf.Abs(rb.velocity.y) > 0.1)
             anim.SetBool("Moving", true);
         else
             anim.SetBool("Moving", false);
