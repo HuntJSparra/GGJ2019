d24cf88b2a64cd20742f6b82d36dd93373cdcd1b
Erik <ecridd@gmail.com>
2019-01-19 22:53:36 -0500

    Started camera and doors

---
+++ The Florists (GGJ2019)/Assets/Doors.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+    	print("This works Erik did something!");
+
+
+    }
+}
