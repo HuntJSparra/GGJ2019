eff0d071bcc24950aa207058c1f08fdd6876742c
Erik <ecridd@gmail.com>
2019-01-20 12:45:29 -0500

    Movement working for flower puzzle

--- The Florists (GGJ2019)/Assets/Interactables/GHPlants.cs
+++ The Florists (GGJ2019)/Assets/Interactables/GHPlants.cs
@@ -7,10 +7,14 @@ public class GHPlants : Interactable {
 
 	public float toMove;	
 	private bool watered;
-	private Vector2[] validPos;
+	public Vector2[] validPos;
+
+	private Rigidbody2D rb;
 
 
-
+	void Start() {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
 
     override public IEnumerator run(Freeroam p) {
     	p.freeze();
@@ -22,12 +26,20 @@ public class GHPlants : Interactable {
     	if (watered) {
     		yield return StartCoroutine(showText("This plant has been watered. It is now too heavy to move."));
     	} else {
+
+    		//if there is nothing in front of us
+    		Rigidbody2D prb = p.GetComponent<Rigidbody2D>();
+
     		for (float distMoved=0;distMoved<toMove;) {
     			float delta = toMove/50;
-	    		transform.Translate(p.direction*delta);
-	    		p.transform.Translate(p.direction*delta);
+    			rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+	    		rb.MovePosition((Vector2)transform.position+p.direction*delta);
+	    		
+	    		prb.MovePosition((Vector2)p.transform.position+p.direction*delta);
 	    		distMoved += delta;
+
 	    		yield return new WaitForSeconds(0.01f);
+	    		rb.constraints = RigidbodyConstraints2D.FreezeAll;
 	    	}
     	}
     }
