d71ff3ecc57043200a0606a61a75db610c499c4b
JordanDeagan <jdeags@gmail.com>
2019-01-20 17:48:28 -0500

    almot done

--- The Florists (GGJ2019)/Assets/Interactables/ViewSis.cs
+++ The Florists (GGJ2019)/Assets/Interactables/ViewSis.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Viewables : Interactable {
+public class ViewSis : Interactable {
 
 	public Sprite image;
 
     override public IEnumerator run(Freeroam p) {
         p.freeze();
         yield return StartCoroutine(showImage(image));
+        yield return StartCoroutine(showText("oh, yeah, my Sister's Birthday is coming up"));
+        EventTracker.foundBday = true;
         p.unfreeze();
     }
 }
