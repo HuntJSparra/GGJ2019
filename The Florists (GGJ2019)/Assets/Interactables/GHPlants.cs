2fb11fa41501d9b018a366cafb0e484766e21b20
Erik <ecridd@gmail.com>
2019-01-20 10:40:42 -0500

    Started work on flower push puzzle

--- The Florists (GGJ2019)/Assets/Interactables/GHPlants.cs
+++ The Florists (GGJ2019)/Assets/Interactables/GHPlants.cs
@@ -5,6 +5,13 @@ using UnityEngine.UI;
 
 public class GHPlants : Interactable {
 
+	public float toMove;	
+	private bool watered;
+	private Vector2[] validPos;
+
+
+
+
     override public IEnumerator run(Freeroam p) {
     	p.freeze();
     	yield return StartCoroutine(pushPlant(p));
@@ -12,6 +19,16 @@ public class GHPlants : Interactable {
     }
 
     IEnumerator pushPlant(Freeroam p){
-    	yield return new WaitForSeconds(5);
+    	if (watered) {
+    		yield return StartCoroutine(showText("This plant has been watered. It is now too heavy to move."));
+    	} else {
+    		for (float distMoved=0;distMoved<toMove;) {
+    			float delta = toMove/50;
+	    		transform.Translate(p.direction*delta);
+	    		p.transform.Translate(p.direction*delta);
+	    		distMoved += delta;
+	    		yield return new WaitForSeconds(0.01f);
+	    	}
+    	}
     }
 }
